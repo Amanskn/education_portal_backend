f0500561eeb2df6be57ee200a36bcf28d935b3c9
Amanskn <29maddhesiaaman@gmail.com>
2024-11-24 19:37:40 +0530

    modified the connecDB function

--- connectDB.js
+++ connectDB.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB(dbUrl, dbName) {
   try {
-    return await mongoose.connect(`${dbUrl}/${dbName}`);
+    return await mongoose.connect(`${dbUrl}`, {
+      dbName,
+    });
   } catch (error) {
     console.log("Error in connectDB.js and the error is: ", error.message);
     throw error;
